e68a751d154d1e31fc2a5ed8fce597363f1121de
Rusydy <rusydymuhiddin@gmail.com>
2023-09-16 16:43:23 +0700

    Style: Re-arrange the position of buttons to align with the updated design
    Previously, the buttons were positioned in a manner that did not conform to the updated design.
    
    This commit re-arranges the button layout to ensure alignment with the updated design, resulting in a more visually appealing and compliant arrangement.

--- app/src/main/java/com/example/counter_app/MainActivity.kt
+++ app/src/main/java/com/example/counter_app/MainActivity.kt
@@ -1,61 +1,152 @@
 package com.example.counter_app
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.*
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 
-@ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                CounterApp()
+                // Use the WebViewLayout composable within your app's content.
+                WebViewLayout(modifier = Modifier.fillMaxSize())
         }
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun CounterApp() {
-    var counter by remember { mutableStateOf(0) }
+fun WebViewLayout(modifier: Modifier) {
+    var popUpVisible by remember { mutableStateOf(false) }
+    var hitCount by remember { mutableStateOf(0) }
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val density = LocalDensity.current.density
+
+    // Define the pop-up content
+    val popUpContent: @Composable () -> Unit = {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .background(Color.White)
+        ) {
+            Text(
+                text = "Pop-up Content",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+            Button(
+                onClick = {
+                    popUpVisible = false
+                    keyboardController?.hide()
+                },
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Text(text = "Close")
+            }
+        }
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+            .padding(16.dp)
     ) {
-        Text(
-            text = "Counter: $counter",
-            style = MaterialTheme.typography.bodyLarge
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(Color.Gray)
+                .pointerInput(Unit) {
+                    detectTapGestures { offset ->
+                        hitCount++
+                        if (hitCount >= 5) {
+                            popUpVisible = true
+                        }
+                    }
+                },
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = if (hitCount < 5) "Hit $hitCount times to show pop-up" else "WEBVIEW",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 16.dp)
+            ) {
+                Button(
+                    onClick = {
+                        // Implement action for the HOME button here
+                        hitCount = 0 // Reset hitCount to 0
+                    },
+                ) {
+                    Text(text = "HOME Button")
+                }
+            }
+
+            // Hidden button that triggers the pop-up
+            Button(
+                onClick = {
+                    popUpVisible = true
+                },
+                modifier = Modifier
+                    .size(0.dp)
+                    .background(Color.Transparent)
+            ) {
+                Text(text = "Hidden Button")
+            }
+        }
+    }
+
+    // Show the pop-up when popUpVisible is true
+    if (popUpVisible) {
+        AlertDialog(
+            onDismissRequest = {
+                popUpVisible = false
+            },
+            title = {
+                Text(text = "Pop-up Content", fontWeight = FontWeight.Bold)
+            },
+            text = {
+                Text(text = "This is your pop-up content.")
+            },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        popUpVisible = false
+                    }
+                ) {
+                    Text(text = "Close")
+                }
+            }
         )
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(
-            onClick = { counter++ },
-            colors = ButtonDefaults.buttonColors(
-                contentColor = Color.Green
-            )
-        ) {
-            Text(text = "Increment")
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(
-            onClick = { counter = 0 },
-            colors = ButtonDefaults.buttonColors(
-                contentColor = Color.Red
-            )
-        ) {
-            Text(text = "Reset")
-        }
     }
 }
