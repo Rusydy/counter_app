a919b70ca864ffaaecb2727fe2ab417c9ecb86fa
Rusydy <rusydymuhiddin@gmail.com>
2023-10-01 18:58:31 +0700

    [CA-24]: Enable Immersive Mode for Full-Screen Experience
    Previously, our app did not offer immersive mode, resulting in the persistent visibility of the system bar.
    
    This commit implements immersive mode, ensuring that the system bar is hidden while the user is interacting with the app.

--- app/src/main/java/com/example/counter_app/MainActivity.kt
+++ app/src/main/java/com/example/counter_app/MainActivity.kt
@@ -3,14 +3,24 @@ package com.example.counter_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
-            WebViewLayout()
+            OptimizedAppLayout()
         }
     }
 }
@@ -18,5 +28,5 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun PreviewWebViewLayout(){
-    WebViewLayout()
+    OptimizedAppLayout()
 }
