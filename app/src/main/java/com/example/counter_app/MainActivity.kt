6dc91894b0df480d6fb61b18f71efa8ff76b2c7f
Rusydy <rusydymuhiddin@gmail.com>
2023-10-14 15:11:23 +0700

    [CA-30]: Enhance Immersive Mode for Kiosk Functionality
    Previously, the immersive mode was ineffective as system bars remained visible when users interacted with the screen.
    
    This commit resolves the issue by overriding specific superclasses and updating the `AndroidManifest.xml` file. These modifications ensure that the immersive mode now functions effectively, creating a more immersive user experience, particularly in Kiosk mode.
    
    The changes enhance the immersive mode, ensuring that system bars are appropriately hidden when users interact with the screen, improving the overall user experience and suitability for Kiosk mode.

--- app/src/main/java/com/example/counter_app/MainActivity.kt
+++ app/src/main/java/com/example/counter_app/MainActivity.kt
@@ -1,28 +1,20 @@
 package com.example.counter_app
 
 import android.os.Bundle
-import android.view.ActionMode
 import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsControllerCompat
 
 @OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setAppImmersiveMode()
-
         setContent {
-            WebViewLayout()
+            OptimizedAppLayout()
         }
     }
 
@@ -63,12 +55,6 @@ class MainActivity : ComponentActivity() {
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 
         actionBar?.hide()
-    }
-
-    private fun setAppImmersiveMode() {
-        window.decorView.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
-
     }
 }
 
